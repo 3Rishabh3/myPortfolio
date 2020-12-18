7d565a113d17433c83fc861096bbd57492c96908
Rishabh Gupta <3rishabh3@gmail.com>
2020-12-18 16:16:20 +0530

    Add files via upload
    Repository added

---
+++ Javascript/slickSlider/script.js
@@ -0,0 +1,73 @@
+$('.sliders1').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 1000, 
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
+
+
+
+$('.sliders2').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  fade: true,
+  cssEase: 'linear',
+  autoplaySpeed: 1200, 
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
